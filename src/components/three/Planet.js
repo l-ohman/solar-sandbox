52b69707a5e60094bf37a057112ae75ca4931bf5
l-ohman <jakelohman7@gmail.com>
2022-09-24 08:41:07 -0500

    tmp

--- src/components/three/Planet.js
+++ src/components/three/Planet.js
@@ -5,25 +5,47 @@ import useStore from "../../store";
 function Planet({ position, distance, speed, color, size, parentDistance = null }) {
   // if 'parentDistance' exists, then 'position' refers to the position of the parent
   const playing = useStore((state) => state.playing);
-  // const [moonPos, setMoonPos] = React.useState([distance + parentDistance, 0, 0]);
-  // const ref = React.useRef();
+  const moonPos = [
+    position[0] + distance,
+    position[1],
+    position[2],
+  ]
+  const ref = React.useRef();
+
+  // https://discourse.threejs.org/t/change-coordinate-origin/16136
+  // check here for more guidance
 
   useFrame(() => {
+    // rotate moon here
     if (parentDistance && playing) {
-      // rotate moon here
+      // currently ref is undefined...?
+      console.log('ref:')
+      console.log(ref);
+      // ref.current.position = position;
     }
   });
 
-  return (
-    <group>
-      {/* moon mesh */}
+  // planet or moon meshes
+  if (!parentDistance) {
+    return(
       <mesh position={position}>
         <sphereGeometry args={[size, 24, 24]}/>
         <meshBasicMaterial color={color} />
       </mesh>
+    );
+  } else {
+    <group ref={ref}>
+      {/* moon mesh */}
+      <mesh>
+        <sphereGeometry args={[size, 24, 24]}/>
+        <meshBasicMaterial color={color} />
+      </mesh>
       {/* centered mesh (for rotation) */}
+      <mesh>
+        <boxBufferGeometry args={[0.1,0.1,0.1]} />
+      </mesh>
     </group>
-  );
+  }
 }
 
 export default Planet;
